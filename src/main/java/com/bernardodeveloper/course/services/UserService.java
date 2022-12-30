0bfb6a7b5f7a9aed192329230e3e6c44f379faf6
gabigol_96@icloud.com <gabigol_96@icloud.com>
2022-12-30 15:39:42 +0000

    Service layer, component registration

---
+++ src/main/java/com/bernardodeveloper/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.bernardodeveloper.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bernardodeveloper.course.entities.User;
+import com.bernardodeveloper.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> userObj = repository.findById(id);
+		return userObj.get();
+	}
+
+}
