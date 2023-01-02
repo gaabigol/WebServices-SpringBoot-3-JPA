55540dbe27dabcf198eafce78ed8871061db8948
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-01-02 22:07:39 +0000

    Order, Instant, ISO 8601

---
+++ src/main/java/com/bernardodeveloper/course/resources/OrderResource.java
@@ -0,0 +1,33 @@
+package com.bernardodeveloper.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bernardodeveloper.course.entities.Order;
+import com.bernardodeveloper.course.services.OrderService;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
+	@Autowired
+	private OrderService service;
+
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order orderObj = service.findById(id);
+		return ResponseEntity.ok().body(orderObj);
+	}
+}
---
+++ src/main/java/com/bernardodeveloper/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.bernardodeveloper.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bernardodeveloper.course.entities.Order;
+import com.bernardodeveloper.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> orderObj = repository.findById(id);
+		return orderObj.get();
+	}
+}
