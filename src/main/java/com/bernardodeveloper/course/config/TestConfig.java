6a24552cb5fab9c9e76b43b6343bce2f5562eaad
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-01-09 20:59:33 +0000

    implemented: class payment and one-To-one association

--- src/main/java/com/bernardodeveloper/course/config/TestConfig.java
+++ src/main/java/com/bernardodeveloper/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.bernardodeveloper.course.entities.Category;
 import com.bernardodeveloper.course.entities.Order;
 import com.bernardodeveloper.course.entities.OrderItem;
+import com.bernardodeveloper.course.entities.Payment;
 import com.bernardodeveloper.course.entities.Product;
 import com.bernardodeveloper.course.entities.User;
 import com.bernardodeveloper.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+		Payment pay1 = new Payment(Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 
 }
