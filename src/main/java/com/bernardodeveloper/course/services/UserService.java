6763dbdd03ac14295456eb1747505c39652dc4c3
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-01-10 21:25:01 +0000

    Exception handling - findById

--- src/main/java/com/bernardodeveloper/course/services/UserService.java
+++ src/main/java/com/bernardodeveloper/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.bernardodeveloper.course.entities.User;
 import com.bernardodeveloper.course.repositories.UserRepository;
+import com.bernardodeveloper.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> userObj = repository.findById(id);
-		return userObj.get();
+		return userObj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User userObject) {
